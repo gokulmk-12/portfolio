d2779da0298eacc044b52df9e4708a27d8944c0b
Gokul M K <gokul@ezio.bbrouter>
2024-06-06 16:54:52 +0530

    Website New Files

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let mixerProjects = mixitup('.projects__container', {
+    selectors: {
+        target: '.project__item',
+    },
+    animation: {
+        duration: 300,
+    },
+});
+
+const linkWork = document.querySelectorAll('.category__btn');
+
+function activeWork(){
+    linkWork.forEach((a) => a.classList.remove('active-work'));
+    this.classList.add('active-work');
+}
+
+linkWork.forEach((a) => a.addEventListener('click', activeWork));
