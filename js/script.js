5a54081d07380059c76b14fa82b65a5b5c117a75
Gokul M K <gokul@ezio.bbrouter>
2024-06-09 17:34:22 +0530

    Updated Experience, Projects

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
-let mixerProjects = mixitup('.projects__container', {
-    selectors: {
-        target: '.project__item',
-    },
-    animation: {
-        duration: 300,
-    },
-});
-
-const linkWork = document.querySelectorAll('.category__btn');
+const linkWork = document.querySelectorAll('.work__item');
 
 function activeWork(){
-    linkWork.forEach((a) => a.classList.remove('active-work'));
+    linkWork.forEach(l=> l.classList.remove('active-work'));
     this.classList.add('active-work');
 }
 
-linkWork.forEach((a) => a.addEventListener('click', activeWork));
+linkWork.forEach(l=> l.addEventListener('click', activeWork));
+
+document.addEventListener("click", (e)=> {
+    if(e.target.classList.contains("work__button")){
+        togglePortfolioPopup();
+        portfolioItemDetails(e.target.parentElement);
+    }
+})
+
+function togglePortfolioPopup(){
+    document.querySelector(".portfolio__popup").classList.toggle("open");
+}
+
+document.querySelector(".portfolio__popup-close").addEventListener("click", togglePortfolioPopup);
+
+function portfolioItemDetails(portfolioItem){
+    document.querySelector(".pp_thumbnail img").src=portfolioItem.querySelector(".work__img").src;
+    document.querySelector(".portfolio__popup-subtitle span").innerHTML = portfolioItem.querySelector(".work__title").innerHTML;
+    document.querySelector(".portfolio__popup-body").innerHTML = portfolioItem.querySelector(".portfolio__item-details").innerHTML;
+}
